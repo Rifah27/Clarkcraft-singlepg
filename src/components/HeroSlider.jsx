4b023a96c07638b8144ec1fd0effa3898638bdf8
Rifah Arif <arifalilko2018@gmail.com>
2025-10-07 18:46:13 +0530

    changes

--- src/components/HeroSlider.jsx
+++ src/components/HeroSlider.jsx
@@ -7,20 +7,7 @@ const Hero = () => {
       className="hero"
       style={{ backgroundImage: `url(${heroImage})` }}
     >
-      {/* <div className="hero-content">
-        <p className="tagline">Yacht Type Variety</p>
-        <h1 className="hero-title">Cruisers</h1>
-        <p className="hero-desc">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, consequatur blanditiis, dolor labore quisquam nulla suscipit soluta ea, ipsam maiores odio porro cumque excepturi rem sint ipsa! Deleniti, quae magnam!
-        </p>
-        <div className="hero-buttons">
-          <button className="btn-outline">VIEW MORE</button>
-          <button className="btn-filled">EXPLORE</button>
-        </div>
-      </div>
-      <div className="arrow-left">&#10094;</div>
-      <div className="arrow-right">&#10095;</div> */}
-    </section>
+  </section>
   );
 };
 
