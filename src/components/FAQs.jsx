54a7bd39b83d65c265d045202600462a62c2284f
Rifah Arif <arifalilko2018@gmail.com>
2025-10-31 21:27:07 +0530

    changes

--- src/components/FAQs.jsx
+++ src/components/FAQs.jsx
@@ -47,7 +47,7 @@ const FAQs = () => {
         <p className="faq-subtitle">BOAT COMPANY</p>
         <h2 className="faq-title">ASK A QUESTION</h2>
         <p className="faq-description">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed similique saepe voluptatibus iusto ea repudiandae sapiente unde minus odit modi labore sunt consequuntur, quas facere ipsam non qui veritatis distinctio!
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed similique saepe voluptatibus iusto ea repudiandae sapiente unde minus odit modi labore sunt consequuntur, quas facere ipsam!
         </p>
       </div>
 
