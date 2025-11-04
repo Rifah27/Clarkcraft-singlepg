449a6faf31a3bd03f280083335b7e5258bd5e3cb
Rifah Arif <arifalilko2018@gmail.com>
2025-11-04 13:33:53 +0530

    changes

--- src/components/Supplies.jsx
+++ src/components/Supplies.jsx
@@ -14,7 +14,7 @@ const yachts = [
   },
   {
     name: "FIBERGLASS & COATINGS",
-    price: "$15 / per day",
+    price: "$20 / per day",
     img: "/assets/fibre-glass.jpeg",
     hoverImg: "/assets/epoxy.jpeg",
     description:
