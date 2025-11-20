904849479f17ba4232d5bc4569d1f1c377179a72
Rifah Arif <arifalilko2018@gmail.com>
2025-11-20 10:24:08 +0530

    changes

--- src/components/Supplies.jsx
+++ src/components/Supplies.jsx
@@ -46,7 +46,7 @@ const yachts = [
   },
   {
     name: "PLAN SAVER",
-    price: "$30/ per day",
+    price: "$20/ per day",
     img: "/assets/plansaver.jpg",
     hoverImg: "/assets/fastener.jpeg",
     description:
