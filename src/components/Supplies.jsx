68c54861eed37471bba4bab801f32a10aa71622b
Rifah Arif <arifalilko2018@gmail.com>
2025-11-21 00:08:30 +0530

    changes

--- src/components/Supplies.jsx
+++ src/components/Supplies.jsx
@@ -22,7 +22,7 @@ const yachts = [
   },
   {
     name: "FASTENERS",
-    price: "$15 / per day",
+    price: "$8 / per day",
     img: "/assets/fastener.jpeg",
     hoverImg: "/assets/epoxy.jpeg",
     description:
