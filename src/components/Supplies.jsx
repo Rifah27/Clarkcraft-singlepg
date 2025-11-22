0ae72a156025352cbe4040f438672955cd254ced
Rifah Arif <arifalilko2018@gmail.com>
2025-11-22 22:18:40 +0530

    changes

--- src/components/Supplies.jsx
+++ src/components/Supplies.jsx
@@ -38,7 +38,7 @@ const yachts = [
   },
   {
     name: "BOOKS & GUIDES",
-    price: "$8/ per day",
+    price: "$12/ per day",
     img: "/assets/book.jpeg",
     hoverImg: "/assets/steering.jpeg",
     description:
