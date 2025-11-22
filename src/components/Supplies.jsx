80475385923d3cf1c1083f73290fd7fca0817c42
Rifah Arif <arifalilko2018@gmail.com>
2025-11-22 22:18:31 +0530

    changes

--- src/components/Supplies.jsx
+++ src/components/Supplies.jsx
@@ -30,7 +30,7 @@ const yachts = [
   },
   {
     name: "STEERING",
-    price: "$8 / per day",
+    price: "$12 / per day",
     img: "/assets/steering.jpeg",
     hoverImg: "/assets/fastener.jpeg",
     description:
