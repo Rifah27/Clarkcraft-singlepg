a6d24496575759fcc5d5ea9897a545e9db8784b8
Rifah Arif <arifalilko2018@gmail.com>
2026-01-31 19:41:46 +0530

    changes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -36,9 +36,9 @@ const AboutUs = () => {
         <h1 className="main-title">ABOUT <strong>CLARK CRAFT</strong></h1>
         <p className="subtitle">
           Since 1957, Clark Craft has believed in one simple idea: anyone with a dream and a solid
-plan can build their own boat. From humble garage beginnings to a trusted name in
-American craftsmanship, we've been empowering builders with proven wooden and
-fiberglass boat designs — not just selling plans, but helping create adventures.
+ plan can build their own boat. From humble garage beginnings to a trusted name in
+ American craftsmanship, we've been empowering builders with proven wooden and
+ fiberglass boat designs — not just selling plans, but helping create adventures.
         </p>
       </div>
 
